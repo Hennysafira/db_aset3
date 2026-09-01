d8dcb42a56091750e7d7fd1aaaea292fa7047752
kalia <kaila.mania45@smk.belajar.id>
2026-09-01 11:00:19 +0700

    Update aset

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\RuanganController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\AsetController;
 
 /*
 |--------------------------------------------------------------------------

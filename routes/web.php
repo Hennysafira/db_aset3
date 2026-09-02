466c30e9d5a2dcb268d085f23d643505b04166be
yusufbljr <yusufblrj53@gmail.com>
2026-09-02 09:54:45 +0700

    cabul

--- routes/web.php
+++ routes/web.php
@@ -64,3 +64,22 @@ Route::put('/aset/{id}', [AsetController::class, 'update'])
 
 Route::delete('/aset/{id}', [AsetController::class, 'destroy'])
     ->name('aset.destroy');
+
+    
+Route::get('/kategori', [KategoriController::class, 'index'])
+    ->name('kategori.index');
+
+Route::get('/kategori/create', [KategoriController::class, 'create'])
+    ->name('kategori.create');
+
+Route::post('/kategori', [KategoriController::class, 'store'])
+    ->name('kategori.store');
+
+Route::get('/kategori/{id}/edit', [KategoriController::class, 'edit'])
+    ->name('kategori.edit');
+
+Route::put('/kategori/{id}', [KategoriController::class, 'update'])
+    ->name('kategori.update');
+
+Route::delete('/kategori/{id}', [KategoriController::class, 'destroy'])
+    ->name('kategori.destroy');
